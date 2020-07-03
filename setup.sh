a15a56cb9b510637a90a7af7c3faac2040d905c9
kriscfoster <krisfoster@live.ie>
2020-07-03 21:19:23 +0100

    started setting up mac mini

--- setup.sh
+++ setup.sh
@@ -1,4 +1,26 @@
-# Developer Tools
-brew install git
+# essentials
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew cask instal google-chrome
+
+# text editors & IDEs
+brew cask install sublime-text
+brew cask install visual-studio-code
+brew cask install intellij-idea-ce
+
+# languages/runtime
+brew install node
+brew install maven
+brew cask install java
+brew cask install pgadmin4
+
+# terminal
 brew cask install iterm2
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# dev ops
 brew install terraform
+brew cask install postman
+
+# manual for now
+# https://www.docker.com/products/docker-desktop
+# https://postgresapp.com/downloads.html
