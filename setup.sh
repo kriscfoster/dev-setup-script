a520a75c87c1ab59d3b38d48945e2624de05e748
Kris Foster <kfoster@kfoster-ltm7.internal.salesforce.com>
2020-10-13 10:56:02 +0100

    IDEs

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@
 brew install git
 brew cask install iterm2
 brew install terraform
+
+brew cask install sublime-text
+brew cask install visual-studio-code
+brew cask install rubymine
+brew cask install intellij-idea
