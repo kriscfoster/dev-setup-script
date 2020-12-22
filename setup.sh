65ad401666694e2ac72ce31b90a6479c688bac69
Kris Foster <kfoster@kfoster-ltm7.internal.salesforce.com>
2020-12-22 12:24:26 +0000

    maven & golang

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@ brew install git
 brew cask install iterm2
 brew install terraform
 
+# Languages & Build Tools
+brew install golang
+brew install node
+brew install maven
+
+# IDEs
 brew cask install sublime-text
 brew cask install visual-studio-code
 brew cask install rubymine
