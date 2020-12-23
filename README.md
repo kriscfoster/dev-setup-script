b8d51e8afeb5504ee42dfe9d5951d4e49632d84b
Kris Foster <kfoster@kfoster-ltm7.internal.salesforce.com>
2020-12-23 11:00:21 +0000

    update

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # dev-setup-script
 
-## Developer Tools
-- iterm2
+- homebrew
 - git
+- iterm2
 - terraform
+- golang
+- node
+- maven
+- java
+- sublime-text
+- visual-studio-code
+- rubymine
+- intellij-idea
+- ohmyzsh
--- setup.sh
+++ setup.sh
@@ -1,15 +1,22 @@
-# Developer Tools
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# developer tools
 brew install git
 brew cask install iterm2
 brew install terraform
 
-# Languages & Build Tools
+# languages & build tools
 brew install golang
 brew install node
 brew install maven
+brew install java
 
 # IDEs
 brew cask install sublime-text
 brew cask install visual-studio-code
 brew cask install rubymine
 brew cask install intellij-idea
+
+# sugar
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
